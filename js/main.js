ef705f3815d2c5fb4177eb51b0d3e933f9c14ac2
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-02 17:08:39 +0100

    bonus con bug 2 click

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 // FUNZIONI
 
-function startGame(board){
-    playButton.addEventListener('click', function(){
-        for ( let i = 1; i <=100; i++){
+function startGame(board, maxNumbers, level){
+        cellContainer.innerHTML ="";
+        for ( let i = 1; i <=maxNumbers; i++){
             const cellNumber = document.createElement("div");
             cellNumber.innerHTML = i;
             cellNumber.classList.add("cellboard");
@@ -12,16 +12,41 @@ function startGame(board){
                 cellNumber.classList.add("active");
                 console.log(i);
             })
+            if (level === "easy") {
+                cellNumber.classList.add("easy");
+            } else if (level === "hard") {
+                cellNumber.classList.add("hard");
+            } else if (level === "hell") {
+                cellNumber.classList.add("hell");
+            }
             board.append(cellNumber);
         }
-    })
 }
 
 //mi prendo il tasto play
-const playButton = document.querySelector(".play");
+const playButton = document.querySelector("button");
 
 // mi prendo la board
 const cellContainer = document.querySelector(".board");
 
-startGame(cellContainer);
+// mi prendo i valori delle options
 
+function getOption() {
+    const selectElement = document.querySelector('#select1');
+    const output = selectElement.value;
+    console.log(output);
+    if ( output === "easy") {
+        playButton.addEventListener('click', function(){
+        startGame(cellContainer, 100, "easy");
+        }
+    )} else if (output === "hard") {
+        playButton.addEventListener('click', function(){
+            startGame(cellContainer, 81, "hard");
+            }
+    )} else {
+        playButton.addEventListener('click', function(){
+            startGame(cellContainer, 49, "hell");
+            }
+    )}
+}
+getOption()
