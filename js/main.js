f3d383c06d9ccff0aba1bb567ec8b3bedc9ad892
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-02 15:18:25 +0100

    fine esercizio base

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 'strict mode'
+
+// FUNZIONI
+
+function startGame(board){
+    playButton.addEventListener('click', function(){
+        for ( let i = 1; i <=100; i++){
+            const cellNumber = document.createElement("div");
+            cellNumber.innerHTML = i;
+            cellNumber.classList.add("cellboard");
+            cellNumber.addEventListener('click', function(){
+                cellNumber.classList.add("active");
+                console.log(i);
+            })
+            board.append(cellNumber);
+        }
+    })
+}
+
+//mi prendo il tasto play
+const playButton = document.querySelector(".play");
+
+// mi prendo la board
+const cellContainer = document.querySelector(".board");
+
+startGame(cellContainer);
+
